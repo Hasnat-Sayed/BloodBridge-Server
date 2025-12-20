3acdc90d4e70cd3d81834b4c3d2e48424cdd7ef0
Hasnat-Sayed <hasnatbin1@gmail.com>
2025-12-20 22:01:33 +0600

    deployed

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         const database = client.db('BloodBridgeDB')
         const userCollections = database.collection('user')
@@ -390,7 +390,7 @@ async function run() {
 
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
